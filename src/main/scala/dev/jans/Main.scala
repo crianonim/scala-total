8cbf1e71eee3e28312aaaf8a294b77019aae2e2d
Jan Skowronski <crianonim@gmail.com>
2022-03-20 11:32:51 -0500

    add pureconfig simeple use

--- src/main/scala/dev/jans/Main.scala
+++ src/main/scala/dev/jans/Main.scala
@@ -1,11 +1,18 @@
 package dev.jans
-
+import pureconfig._
+import pureconfig.generic.auto._
 import cats.effect.{ExitCode, IO, IOApp}
+import pureconfig.ConfigReader.Result
 
 object Main extends IOApp {
+  case class Config (username:String,password:String)
+  val loadedConfig: Result[Config] =ConfigSource.default.load[Config]
+
   override def run(args: List[String]): IO[ExitCode] = {
-    IO {
-      println("jan")
-    }.flatMap(_ => IO(ExitCode.Success))
+   val msg = loadedConfig match  {
+      case Right(config) => config
+      case Left(failures) => failures
+    }
+    IO { println(msg)} .flatMap(_ => IO(ExitCode.Success))
   }
 }
